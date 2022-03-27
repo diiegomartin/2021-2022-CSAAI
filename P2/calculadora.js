afc830e455782a40dab0ceafe883fa6a66bce3e7
diiegomartin <d.martingr.2019@alumnos.urjc.es>
2022-03-27 20:37:26 +0200

    calculadora

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -2,107 +2,59 @@
 console.log("Ejecutando JS...");
 
 
-//-- Elementos de la interfaz de la calculadora
 display = document.getElementById("display")
-boton1 = document.getElementById("boton1")
-boton2 = document.getElementById("boton2")
-boton3 = document.getElementById("boton3")
-boton4 = document.getElementById("boton4")
-boton5 = document.getElementById("boton5")
-boton6 = document.getElementById("boton6")
-boton7 = document.getElementById("boton7")
-boton8 = document.getElementById("boton8")
-boton9 = document.getElementById("boton9")
-boton0 = document.getElementById("boton0")
-
-suma = document.getElementById("suma")
-resta = document.getElementById("resta")
-multiplicacion = document.getElementById("multiplicacion")
-division = document.getElementById("division")
-igual = document.getElementById("igual")
 clear = document.getElementById("clear")
+borrar = document.getElementById("borrar")
+igual = document.getElementById("igual")
 
-//-- Funciones de retrollamada de los botones
-//-- Cada vez que se aprieta un botón se actúa
-//-- sobre la cadena: añadiendo digito, operador +
-//-- poniendo a cero o evaluando la expresión
+//-- Crear un array con todos los elementos
+//-- de la clase digito
+digito = document.getElementsByClassName("digito")
 
-// -- Insertar digito 1
-boton1.onclick = () => {
-  display.innerHTML += "1";
+for (i=0; i<digito.length; i++) {
+  digito[i].onclick = (ev) => {
+    if (display.innerHTML == "0"){
+      display.innerHTML = ev.target.value;
+    }else{
+      display.innerHTML += ev.target.value;
+    }
+  }
 }
 
-//-- Insertar digito 2
-boton2.onclick = () => {
-  display.innerHTML += "2";
-}
+//-- Crear un array con todos los elementos
+//-- de la clase operador
+operador = document.getElementsByClassName("operador")
 
-//-- Insertar digito 3
-boton3.onclick = () => {
-  display.innerHTML += "3";
-}
-
-//-- Insertar digito 4
-boton4.onclick = () => {
-  display.innerHTML += "4";
-}
-
-//-- Insertar digito 5
-boton5.onclick = () => {
-  display.innerHTML += "5";
-}
-
-//-- Insertar digito 6
-boton6.onclick = () => {
-  display.innerHTML += "6";
-}
-
-//-- Insertar digito 7
-boton7.onclick = () => {
-  display.innerHTML += "7";
-}
-
-//-- Insertar digito 8
-boton8.onclick = () => {
-  display.innerHTML += "8";
-}
-
-//-- Insertar digito 9
-boton9.onclick = () => {
-  display.innerHTML += "9";
-}
-
-//-- Insertar digito 0
-boton0.onclick = () => {
-  display.innerHTML += "0";
-}
-
-//-- Insertar simbolo de sumar
-suma.onclick = () => {
-  display.innerHTML += "+";
-}
-
-//-- Insertar simbolo de restar
-resta.onclick = () => {
-  display.innerHTML += "-";
-}
-
-//-- Insertar simbolo de multiplicar
-multiplicacion.onclick = () => {
-  display.innerHTML += "*";
-}
-
-//-- Insertar simbolo de division
-division.onclick = () => {
-  display.innerHTML += "/";
+for (i=0; i<operador.length; i++) {
+  operador[i].onclick = (ev) => {
+    if (display.innerHTML == "0"){
+      display.innerHTML = ev.target.value;
+    }else{
+      display.innerHTML += ev.target.value;
+    }
+  }
 }
 
 //-- Evaluar la expresion
 igual.onclick = () => {
   display.innerHTML = eval(display.innerHTML);
+  click.play();
 }
 
 //-- Poner a cero la expresion
 clear.onclick = () => {
   display.innerHTML = "0";
+  click.play();
+}
+
+//-- Borrar el ultimo numero u operador
+//-- introducido en el array
+borrar.onclick = () => {
+  if (display.innerHTML == "0"){
+    display.innerHTML = "0";
+  }else if (display.innerHTML == ""){
+    display.innerHTML = "0";
+  }else{
+    display.innerHTML = display.innerHTML.slice(0,-1)
+  }
 }
