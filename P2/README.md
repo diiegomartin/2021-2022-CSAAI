5ea57dde401b5c4c29572d76d31001c9fc79237c
diiegomartin <d.martingr.2019@alumnos.urjc.es>
2022-03-28 23:39:56 +0200

    CALCULADORA FIN

--- P2/README.md
+++ P2/README.md
@@ -1,1 +1,19 @@
  # Práctica 2
+
+DIEGO MARTÍN GRANJA
+
+ HTML
+ Respecto al HTML, es un código bastante básico con distintas divisiones para poder mover la disposición de la calculadora, de los botones y de la pantalla o display. He usado también clases e identificadores en los botones y en los operadores para usarlos en el código de javascript que explicaré posteriormente. Por último quise poner un párrafo con la marca 'CASIO' de mi calculadora.
+
+ CSS
+ Respecto a CSS primero he querido usar una fuente diferente a la que viene por defevto en html, asi que me descargué la mostrada en el código por internet. Luego puse un fonto de una mesa con materiales de trabajo y coloqué la calculadora en ese lugar mediante el comando 'border' dentro del div de calculadora.
+ Continué dándole forma y estilo a los botones y al display con márgenes, colores, fondos de imágenes, la fuente descargada nombrada previamente, cambio de tamaño ...
+
+ JAVASCRIPT
+ Respecto al JavaScript, lo primero que hemos hecho ha sido llamar a las clases e identificadores del código html.
+ Una vez llamados, hemos creado constantes de estado, que nos ayudarán a situarnos para saber si el siguiente paso es un dígito o un operador.
+ Después hemos hecho dos bucles for que recorren y reconocen el array de dígitos y de signos.
+ Tras los bucles, hemos creado una función para los dígitos, que básicamente con ayuda de los estados, son los que hacen que unicamente podamos hacer una operación con un único operador como se pide en la práctica.
+ La otra función mostrada es la de los operadores, que tiene una mejora, y es para poder comenzar con unnúmero negativo a operar. Esto lo he conseguido gracias al identificador 'resta'.
+ Por último he usado los comandos mostrados en el código para realizar las funciones de 'borrado de un dígito/operador', 'reinicio' y 'mostrar el resultado' al pulsar los botones respectivos.
+
--- P2/calculadora.js
+++ P2/calculadora.js
@@ -1,23 +1,34 @@
 
 console.log("Ejecutando JS...");
 
-
+// LLamamos a los elementos identificadores del HTML
 display = document.getElementById("display")
 clear = document.getElementById("clear")
 borrar = document.getElementById("borrar")
 igual = document.getElementById("igual")
+restar = document.getElementById("restar")
+
 
 //-- Crear un array con todos los elementos
 //-- de la clase digito
+
+// ESTADOS DE 
+
+const ESTADO = {
+  inicial : 0,
+  primerdigito : 1,
+  signo : 2,
+  segundodigito : 3,
+}
+
+
+
+
 digito = document.getElementsByClassName("digito")
 
 for (i=0; i<digito.length; i++) {
   digito[i].onclick = (ev) => {
-    if (display.innerHTML == "0"){
-      display.innerHTML = ev.target.value;
-    }else{
-      display.innerHTML += ev.target.value;
-    }
+  numeros(ev.target.value);
   }
 }
 
@@ -27,34 +38,69 @@ operador = document.getElementsByClassName("operador")
 
 for (i=0; i<operador.length; i++) {
   operador[i].onclick = (ev) => {
-    if (display.innerHTML == "0"){
-      display.innerHTML = ev.target.value;
-    }else{
-      display.innerHTML += ev.target.value;
-    }
+  operation(ev.target.value);
   }
 }
+
+
+
+
+let state = ESTADO.inicial;
+
+function operation(operador){
+  if (state == ESTADO.inicial){ /* Para poder poner un menos al empezar */
+      restar.onclick = () => {
+        display.innerHTML += "-";
+        state = ESTADO.primerdigito;
+      }
+  }
+  else if (state == ESTADO.primerdigito){
+    display.innerHTML += operador;
+    state = ESTADO.signo;
+  }
+}
+
+function numeros(digito){
+  if (state == ESTADO.inicial){
+    display.innerHTML = digito;
+    state = ESTADO.primerdigito;
+  }
+  else if (state == ESTADO.primerdigito){
+    display.innerHTML += digito;
+  }
+  else if (state == ESTADO.signo){
+    display.innerHTML += digito;
+    state = ESTADO.segundodigito;
+  }
+  else if (state == ESTADO.segundodigito){
+    display.innerHTML += digito;
+  }
+}
+
 
 //-- Evaluar la expresion
 igual.onclick = () => {
   display.innerHTML = eval(display.innerHTML);
-  click.play();
+  state = ESTADO.primerdigito;
+  
 }
 
-//-- Poner a cero la expresion
+/* FUNCIÓN PARA ELIMINAR TODO LO DEL DISPLAY 'CLEAR' */
 clear.onclick = () => {
   display.innerHTML = "0";
-  click.play();
+  state = ESTADO.inicial;
 }
 
-//-- Borrar el ultimo numero u operador
-//-- introducido en el array
+/* BORRAR EL ÚLTIMO NÚMERO U OPERADOR */
+
 borrar.onclick = () => {
-  if (display.innerHTML == "0"){
-    display.innerHTML = "0";
-  }else if (display.innerHTML == ""){
-    display.innerHTML = "0";
-  }else{
-    display.innerHTML = display.innerHTML.slice(0,-1)
-  }
+  /* Al borrar el primer dígito */
+    if (state == ESTADO.primerdigito || state == ESTADO.segundodigito){
+      display.innerHTML = display.innerHTML.slice(0,-1)
+    }
+    /* Al borrar el signo */
+    else if (state == ESTADO.signo){
+      display.innerHTML = display.innerHTML.slice(0,-1)
+      state = ESTADO.primerdigito;
+    }
 }
